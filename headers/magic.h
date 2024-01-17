d6391b2b9ba4f3bd4c8cb4d15b73e919618f4a6f
vvsalash <vova.salash609@gmail.com>
2024-01-17 17:06:09 +0300

    Described methods of the data loader

--- headers/magic.h
+++ headers/magic.h
@@ -5,4 +5,7 @@
 namespace magic {
     constexpr int8_t kMaxDimCount = 4;
     constexpr int32_t kUndefinedSize = -1;
+    constexpr uint32_t kMnistRows = 28;
+    constexpr uint32_t kMnistCols = 28;
+
 }  // namespace magic
